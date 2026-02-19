ad88004b514e966ad381beec397aab3f26e715ab
Taras <8crystal7@gmail.com>
2026-02-19 22:18:24 +0000

    began working on data transfer between client and server and created a to-do list

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session
 from agent import Agent
 from main import Player, Game
 
 app = Flask(__name__)
+app.secret_key = 'HelloWorld!'
+
+Player1 = None
+Player2 = None
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 
 
 @app.route('/play-against-bot', methods=['GET', 'POST'])
-def index():
-    return render_template('play-against-bot.html')
+def play_against_bot():
+
+    global Player1 
+    Player1 = Player(request.form['username'])
+    global Player2
+    Player2 = Agent('bot')
+
+    return render_template('play-against-bot.html',
+                           name = request.form['username'],
+                           game_data = {
+                               'player1l': Player2.left.get_value(),
+                               'player1r': Player2.right.get_value(),
+                               'player2l': Player1.left.get_value(),
+                               'player2r': Player1.right.get_value()
+                           })
+
+
+
+@app.route('/play-against-player', methods=['GET', 'POST'])
+def play_against_player():
+    return render_template('play-against-player.html')
 
 
 
